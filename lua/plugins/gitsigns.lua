0cfe77122a7ad324395ea96ed8fa6d1668fba04e
Devanand Sharma <devanandinquiries@gmail.com>
2024-11-07 21:27:25 -0500

    Add gitsigns to config

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,39 @@
+return {
+	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		signs = {
+			add = { text = "│" },
+			change = { text = "│" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		signcolumn = true,
+		numhl = false,
+		linehl = false,
+		word_diff = false,
+		watch_gitdir = {
+			interval = 1000,
+			follow_files = true,
+		},
+		attach_to_untracked = true,
+		current_line_blame = true,
+		current_line_blame_opts = {
+			virt_text = true,
+			virt_text_pos = "eol",
+			delay = 1000,
+		},
+		sign_priority = 6,
+		update_debounce = 100,
+		status_formatter = nil,
+		preview_config = {
+			border = "single",
+			style = "minimal",
+			relative = "cursor",
+			row = 0,
+			col = 1,
+		},
+	},
+}
